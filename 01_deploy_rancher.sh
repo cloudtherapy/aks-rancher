0d241bedded31661ea5ba28f37bd7e578fafc892
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2024-12-07 23:47:09 -0500

    Initial commit

---
+++ 01_deploy_rancher.sh
@@ -0,0 +1,12 @@
+. ./environment
+
+az group create --name rg-${APP} --location ${LOCATION}
+
+az aks create \
+  --resource-group rg-${APP} \
+  --name ${APP}-server \
+  --kubernetes-version ${VERSION} \
+  --node-count ${NODE_COUNT} \
+  --node-vm-size $VM_SIZE
+
+  az aks get-credentials --resource-group rg-${APP} --name ${APP}-server
